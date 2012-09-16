b4d06beabf30b71beb8e21be0e1028c967c06f68
Daniel Kaplan <dtkaplan@gmail.com>
2012-09-16 15:06:54 -0500

    misc. adds

---
+++ Software/m155development.R
@@ -0,0 +1,12 @@
+listFun <- function(...) {
+  dots <- list(...,..=NA)
+  nm <- names(dots)
+  f <- function(x) {
+    y <- x <- as.character(x)
+    y[!(x %in% nm)] <- ".."
+    r <- unlist(dots[y])
+    names(r) <- x
+    return(r)
+    }
+    return(f)
+}
