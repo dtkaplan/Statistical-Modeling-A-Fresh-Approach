039d48026418bf94ed8fe40dbc4ff2fd9b2843d8
Daniel Kaplan <dtkaplan@gmail.com>
2012-10-06 09:31:14 -0500

    Notes for Monday 8 Oct.

--- Software/m155development.R
+++ Software/m155development.R
@@ -1,3 +1,5 @@
+# Make a function based on a list of discrete values
+
 listFun <- function(...) {
   dots <- list(...,..=NA)
   nm <- names(dots)
@@ -12,3 +14,33 @@ listFun <- function(...) {
 }
 # Set the handling of NAs
 options( na.rm=TRUE)
+
+# Calculate the angle between two vectors or, more generally,
+# between a vector and a subspace defined by a model
+# Remember to suppress the intercept
+angle <- function( object, v2, data=NULL, degrees=TRUE ) UseMethod("angle")
+# =============
+angle.default <- function(object, ... ) {
+  stop("First argument should be a model, a formula, or a numerical vector.")
+}
+# ============
+angle.numeric <- function(object, v2, data=NULL, degrees=TRUE){
+  ang <- acos(sum(object*v2)/sqrt(sum(object^2)*sum(v2^2)))
+  if (degrees) ang <- 180*ang/pi
+  return(ang)
+}
+# =============
+angle.logical <- function(object, v2, data=NULL, degrees=TRUE ) {
+  angle.numeric( as.numeric(object), as.numeric(v2), degrees=degrees)
+}
+# =============
+angle.formula <- function( object, v2, data=NULL, degrees=TRUE) {
+  mod <- lm(object, data=data)
+  angle.lm(mod,degrees=degrees)
+}
+# =============
+angle.lm <- function(object, v2, data=NULL, degrees=TRUE) {
+  # response is the first component of the "model" component
+  response <- object$model[,1]
+  angle.numeric(response,fitted(object), degrees=degrees)
+}
