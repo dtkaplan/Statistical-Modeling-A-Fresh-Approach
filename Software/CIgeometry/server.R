f8ae05ec630a54a6f52aa1e03c476bac8dbcb9f1
Daniel Kaplan <dtkaplan@gmail.com>
2012-10-28 14:09:57 -0500

    CIgeometry app

--- Software/CIgeometry/server.R
+++ Software/CIgeometry/server.R
@@ -1,34 +1,80 @@
 library(shiny)
 library(datasets)
 
-#resid.ang = rnorm(1,mean=pi/2,sd=pi/6)*sign(rnorm(1)) #Should stay the same except for new data
+.libPaths('~/R/library')
 
 # Define server logic required to plot various variables against mpg
 shinyServer(function(input, output) {
-  drawNewAngle <- reactive(function(){
+  newRandomDir <- reactive(function(){
     x <- input$newvector # just trigger the function
-     rnorm(1,mean=pi/2,sd=pi/6)*sign(rnorm(1))
+    ang = runif(1,min=0,max=2*pi)
+    r = rnorm(1)
+    c(r*cos(ang),r*sin(ang),sign(rnorm(1)))
    })
-  # Generate the plot 
+  newRandomVec <- reactive(function(){
+    input$res.sd*newRandomDir()*c(c(1,1)/sqrt(input$n),sqrt(1-1/input$n)) # in the deterministic plane
+  })
+    
+  # Generate the plot
   output$ciPlot <- reactivePlot(function() {
    drawSamplingGeometry(bcoef=input$bcoef,ccoef=input$ccoef,
                         theta=input$theta,res.sd=input$res.sd,
                         n=input$n,
                         bunits=input$blength,cunits=input$clength,
+                        showBnaive=input$showBnaive,
                         showBcontours=input$showBcontours,
                         showCcontours=input$showCcontours,
-                        showRandom=input$showRandom,
-                        resid.ang = drawNewAngle()
+                        showRandom=FALSE,
+                        noise = newRandomVec()
+                        )
+  })
+  output$ciMeasPlot <-  reactivePlot(function() {
+   drawSamplingGeometry(bcoef=input$bcoef,ccoef=input$ccoef,
+                        theta=input$theta,res.sd=input$res.sd,
+                        n=input$n,
+                        bunits=input$blength,cunits=input$clength,
+                        showBnaive=input$showBnaive,
+                        showBcontours=input$showBcontours,
+                        showCcontours=input$showCcontours,
+                        showRandom=TRUE,
+                        noise = newRandomVec(),
+                        showSample=TRUE,
+                        showSamplingDist=FALSE
+                        )
+  })
+  output$ci3D <- reactivePlot(function(){
+    if( require(scatterplot3d,quietly=TRUE)) {
+      draw3dview(bcoef=input$bcoef, ccoef=input$ccoef, theta=input$theta,
+               res.sd=input$res.sd, n=input$n,noise=newRandomVec(),
+                 showResamps=input$showSimulation)
+    } else  stop("Must install scatterplot3d package")
+  })
+    
+  output$ciDetermPlot <-  reactivePlot(function() {
+   drawSamplingGeometry(bcoef=input$bcoef,ccoef=input$ccoef,
+                        theta=input$theta,res.sd=input$res.sd,
+                        n=input$n,
+                        bunits=1, cunits=1, # Always show in natural units
+                        #bunits=input$blength,cunits=input$clength,
+                        showBnaive=input$showBnaive,
+                        showBcontours=input$showBcontours,
+                        showCcontours=input$showCcontours,
+                        showRandom=FALSE,
+                        noise = newRandomVec(),
+                        showSample=FALSE,
+                        showSamplingDist=FALSE
                         )
   })
 })
 
 # Drawing Program =================================
 drawSamplingGeometry <- function(bcoef=1.5, ccoef=NULL, theta=90, res.sd=2, n=10,
-                                 bunits=1,cunits=1,
+                                 bunits=1,cunits=1, showBnaive=TRUE,
                                  showBcontours=TRUE, showCcontours=FALSE,
                                  showRandom=TRUE,
-                                 resid.ang=pi/2.3) {
+                                 showSamplingDist=TRUE,
+                                 showSample=TRUE,
+                                 noise=c(.1,-.2) ){
   bunits=as.numeric(bunits); cunits=as.numeric(cunits)
   bvec = c(1.3,.5)*bunits
   # absolute direction of covariate vector
@@ -39,13 +85,24 @@ drawSamplingGeometry <- function(bcoef=1.5, ccoef=NULL, theta=90, res.sd=2, n=10
         xlab="",ylab="")
   arrows(0,0,bvec[1],bvec[2],lwd=3, col="blue",length=.1)
   lines( c(-10,10)*bvec[1], c(-10,10)*bvec[2], col="blue", lty=2 )
+  lines( c(-10,10)*cvec[1], c(-10,10)*cvec[2], col="green", lty=2 )
   if(!is.null(ccoef)) arrows(0,0,cvec[1],cvec[2],col="green",lwd=3,length=.1)
   # Draw the deterministic value
   Apos = bcoef*bvec/bunits
   if(!is.null(ccoef)) Apos = Apos + ccoef*cvec/cunits
-  points(Apos[1],Apos[2],pch=20,col="black")
+  points(Apos[1],Apos[2],pch=3,col="black") # The deterministic value
   text(Apos[1],Apos[2],"A",pos=4)
 
+  if( showBnaive ) { # Draw the bcoef contours without accounting for the covariate
+     direction = atan2(bvec[2],bvec[1]) + pi/2 # perpendicular to B
+     for(k in -40:40) {
+      lines( c(-1000,1000)*cos(direction)+k*bvec[1], 
+             c(-1000,1000)*sin(direction)+k*bvec[2],lty=3,col="blue")
+      position = k*bvec + 2*c(cos(direction),sin(direction))
+      text( position[1],position[2],k,col="blue")
+    }
+  }
+  
   if( showBcontours ) {   # Draw the bcoef coordinate contours
     if(is.null(ccoef)) {# no C vector
       direction = atan2(bvec[2],bvec[1]) + pi/2 # perpendicular to B
@@ -72,19 +129,74 @@ drawSamplingGeometry <- function(bcoef=1.5, ccoef=NULL, theta=90, res.sd=2, n=10
   }
   # draw the sample value
  
-  Ahat = Apos + res.sd*c(cos(resid.ang),sin(resid.ang))
+  Ahat = Apos + noise[c(1,2)]
   if( showRandom) arrows(Apos[1],Apos[2],Ahat[1],Ahat[2],col=rgb(1,0,0,.5),lwd=5,length=.1)
-  points(Ahat[1],Ahat[2],pch=20,col="black")
-  text(Ahat[1],Ahat[2],expression(hat(A)),pos=ifelse(Ahat[2]>0,3,1))
-  # draw the residual radius
-  angs = seq(0,2*pi,length=100)
-  xpts = Ahat[1] + res.sd*cos(angs)
-  ypts = Ahat[2] + res.sd*sin(angs)
-  polygon(xpts,ypts,col=rgb(1,0,0,.05),border=NA)
-  # draw more density where the random vectors are likely to be
-  for (scaling in c(1.96,1.33,1,.7,.44)){
-    xpts = Ahat[1] + scaling*res.sd*cos(angs)/sqrt(n)
-    ypts = Ahat[2] + scaling*res.sd*sin(angs)/sqrt(n)
-    polygon(xpts,ypts,col=rgb(1,0,0,.2),border=NA)
+  if( showSample ) {
+    points(Ahat[1],Ahat[2],pch=20,col="black")
+    points(Ahat[1],Ahat[2],pch=1,cex=2,col="black")
   }
+  if( showSamplingDist ) {# draw the residual radius
+    angs = seq(0,2*pi,length=100)
+    xpts = Ahat[1] + res.sd*cos(angs)
+    ypts = Ahat[2] + res.sd*sin(angs)
+    polygon(xpts,ypts,col=rgb(1,0,0,.05),border=NA)
+    # draw more density where the random vectors are likely to be
+    for (scaling in c(1.96,1.33,1,.7,.44)){
+      xpts = Ahat[1] + scaling*res.sd*cos(angs)/sqrt(n)
+      ypts = Ahat[2] + scaling*res.sd*sin(angs)/sqrt(n)
+      polygon(xpts,ypts,col=rgb(1,0,0,.2),border=NA)
+  }
+  }
+}
+# ========
+draw3dview <- function(bcoef=1.5, ccoef=2, theta=90, res.sd=2, n=10,noise=rep(1,3),showResamps=TRUE) {
+  bvec = c(1.3,.5)
+  # absolute direction of covariate vector
+  cang = atan2(bvec[2],bvec[1]) + theta*pi/180 
+  cvec = c(1.5*cos(cang),1.5*sin(cang))
+  par( bty="n") # no box
+  z = seq(-1,3,length=100)
+  x = cos(z)
+  y = sin(z)
+  pframe = scatterplot3d(x,y,z,main="3-D View",xlab="",ylab="", grid=FALSE,
+                axis=FALSE,zlim=c(-3,5),xlim=c(-5,5),ylim=c(-5,5),type="n")
+  pframe$plane3d(0,0,0, lty=1,col="gray")
+  pframe$points3d(c(0,bvec[1]),c(0,bvec[2]),c(0,0),type="l",lwd=3, col="blue")
+  pframe$points3d(c(-3,3)*bvec[1],c(-3,3)*bvec[2], c(0,0),
+                  type="l", col="blue",lty=2)
+  pframe$points3d(c(0,cvec[1]),c(0,cvec[2]),c(0,0),type="l",lwd=3, col="green")
+  pframe$points3d(c(-3,3)*cvec[1],c(-3,3)*cvec[2], c(0,0),
+                  type="l", col="green",lty=2)
+  Adet = bcoef*c(bvec,0) + ccoef*c(cvec,0)
+  Apos = Adet + noise
+  projectColor = ifelse(Apos[3]>0, rgb(.3,.3,.3),rgb(.3,.3,.3,.4))
+  pframe$points3d(c(1,1)*Apos[1],c(1,1)*Apos[2],c(0,Apos[3]),col=projectColor,lwd=3,type="l")
+  pframe$points3d(Apos[1],Apos[2],Apos[3],pch=20,col="black")
+  pframe$points3d(Apos[1],Apos[2],0,pch=1,cex=1,col="black")
+  resid.color = rgb(1,0,0,ifelse( Apos[3]>0,1,.3))
+  pframe$points3d(c(Adet[1],Apos[1]),c(Adet[2],Apos[2]), c(0,Apos[3]), 
+                  col="red", lwd=3, type="l")
+  pframe$points3d(Adet[1],Adet[2],0,pch=4,cex=1,col="black")
+
+  # Show the resampling variation
+  if( showResamps ){
+    for (k in 1:50) {
+      newx = Apos[1] + rnorm(1)*sqrt(1/n)*res.sd/sqrt(3)
+      newy = Apos[2] + rnorm(1)*sqrt(1/n)*res.sd/sqrt(3)
+      newz = Apos[3] + rnorm(1)*res.sd/sqrt(3)
+      pframe$points3d(newx,newy,newz,
+                    col=rgb(1,0,0,.2), pch=20)
+      pframe$points3d(newx, newy, 0,
+                    col=rgb(1,0,0,.4), pch=1)
+      pframe$points3d(c(Apos[1], newx),
+                    c(Apos[2], newy),
+                    c(Apos[3], newz),
+                    col=rgb(1,0,0,.2), type="l")
+      pframe$points3d(c(newx, newx),
+                    c(newy, newy),
+                    c(0, newz),
+                    col=rgb(0,0,0,.2), type="l")
+    }
+  }
+    
 }
--- Software/CIgeometry/ui.R
+++ Software/CIgeometry/ui.R
@@ -2,30 +2,49 @@ library(shiny)
 
 # Define UI for miles per gallon application
 shinyUI(pageWithSidebar(
-
+ 
   # Application title
-  headerPanel("Geometry of Confidence Intervals"),
+  headerPanel("Geometry of Covariation and Sampling"),
   sidebarPanel(
-    helpText("Deterministic Part of System: vector A"),
-    numericInput("bcoef", "Coef. on B (med. units)", value=2,min=-2.5,max=2.5,step=.25),
-    numericInput("ccoef", "Coef. on C (med. units)", value=0,min=-3,max=3,step=.25),
-    sliderInput("theta","Angle of colinearity:", min=0,max=180,value=90),
-    helpText("Random Part of Model"),           
-    sliderInput("res.sd", "Standard Deviation of Noise:", min=.1,max=4,value=1,step=.1),
-    checkboxInput("newvector", "Generate new noise.",value=FALSE),
-    helpText("Data Collection"),    
-    numericInput("n", "Number of Cases:", value=10, min=2,max=10000),
-    selectInput("blength","Units of B measurement:", list(Med=1,Small=.5,Big=1.7)),
-    selectInput("clength","Units of C measurement:", list(Med=1,Small=.5,Big=1.7)),
-    helpText("Display Choices"),
-    checkboxInput("showBcontours", "Show B coef contours", FALSE ),
-    checkboxInput("showCcontours", "Show C coef contours", FALSE ),
-    checkboxInput("showRandom", 'Show measurement "noise"', TRUE )
+    helpText("Factors to Control"),
+    numericInput("n", "Measurement - Sample Size n:", value=10, min=2,max=10000),
+    sliderInput("res.sd", "Standard Deviation of Noise:", min=.1,max=4,value=2.5,step=.1),   
+    sliderInput("theta","Deterministic: Angle of Colinearity:", min=2,max=178,value=90),     
+    helpText("Model A ~ B"),
+    checkboxInput("showBnaive", "B coef contours", FALSE ),
+    helpText("Model A ~ B + C"), 
+    checkboxInput("showBcontours", "B coef contours in A ~ B + C", FALSE ),
+    checkboxInput("showCcontours", "C coef contours in A ~ B + C", FALSE ),
+    div(HTML("Audio introduction")),
+    div(HTML('<iframe width="250" height="40" src="http://www.youtube.com/embed/yDPfEaY-j9E?rel=0&autohide=0&modestbranding=1" frameborder="0"></iframe>'))            
   ),
-                          # Show the caption and plot of the requested variable against mpg
   mainPanel(
-    h3(textOutput("caption")),
-
-    plotOutput("ciPlot",width="500px",height="500px")
-  )
+     tabsetPanel(
+        tabPanel("Deterministic",        
+          helpText("Deterministic Part of System: vector A."),
+          div(HTML('Narration: <iframe width="300" height="40" src="http://www.youtube.com/embed/FPjjFAFBHaQ?rel=0&autohide=0&modestbranding=1" frameborder="0"></iframe>')),
+          numericInput("bcoef", HTML("<span style='color:blue;'>Coef. on B</span>"), value=2,min=-2.5,max=2.5,step=.25),
+          numericInput("ccoef", HTML("<span style='color:green;'>Coef. on C</span>"), value=0,min=-3,max=3,step=.25),
+          plotOutput("ciDetermPlot",width="500px",height="500px")
+        ),
+        tabPanel("Measurement",
+          div(HTML('Narration: <iframe width="300" height="40" src="http://www.youtube.com/embed/HNcSinU1HTU?rel=0&autohide=0&modestbranding=1" frameborder="0" allowfullscreen></iframe>')),
+          checkboxInput("newvector", HTML("<span style='color:red'>Generate new sample.</span>"),value=FALSE), 
+          selectInput("blength","Units of B measurement:", list(Med=1,Small=.5,Big=1.7)),
+          selectInput("clength","Units of C measurement:", list(Med=1,Small=.5,Bg=1.7)),
+          plotOutput("ciMeasPlot",width="500px",height="500px")
+        ),
+        tabPanel("3-D View",
+           div(HTML('Narration: <iframe width="300" height="40" src="http://www.youtube.com/embed/b06-G799Zmc?rel=0&autohide=0&modestbranding=1" frameborder="0" allowfullscreen></iframe>')),
+           checkboxInput("newvector", HTML("<span style='color:red'>Generate new sample.</span>"),value=FALSE),
+          checkboxInput("showSimulation", "Show CI simulation", value=FALSE),
+          plotOutput("ci3D", width="500px",height="500px")
+        ),
+        tabPanel("Conf. Interval",
+          div(HTML('Narration: <iframe width="300" height="40" src="http://www.youtube.com/embed/9aalyMDN2j4?rel=0&autohide=0&modestbranding=1" frameborder="0"></iframe>')),
+          checkboxInput("newvector", HTML("<span style='color:red'>Generate new sample.</span>"),value=FALSE),
+          plotOutput("ciPlot",width="500px",height="500px")
+        )
+     )
+   )        
 ))
